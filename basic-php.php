c570b664b994e2959c3e6a5c733c3000d983cb51
Ritesh Maurya <maurya.ritesh2005@gmail.com>
2026-01-11 00:36:53 +0530

    learning arithmatic

--- basic-php.php
+++ basic-php.php
@@ -5,25 +5,46 @@ echo 10 + 10 . "<br>";
 
 // varibalres --> A variables are container for storing data values in php  variable name start with a dollar sign($) followed by the name of variable 
 $name = "Ritesh Maurya <br>"; // string 
-echo "My name is " . $name; 
+echo "My name is " . $name;
 
 // different data types in php
 $age = 24; // integer
 $cgpa = 8.5; // float 
 $isStudent = true; // boolean 
 
+// Arithmatic operator 
+// + - * / ** %
+$x = 10;
+$y = 2;
+echo "Sum of $x and $y is " . ($x + $y) . "<br>";
+echo "minus of $x and $y is ", ($x - $y) . "<br>";
+echo "product of $x and $y is ", ($x * $y) . "<br>";
+echo "exponent of $x and $y is ", ($x ** $y) . "<br>";
+echo "divide of $x and $y is ", ($x / $y) . "<br>";
+// increment and decrement operator 
+
+$count = 10;
+$count += 5;
+echo $count;
+
+
+
+
 
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>learning php</title>
 </head>
+
 <body>
     <h1>to learn php click the button</h1>
     <button>click me</button>
 </body>
+
 </html>
