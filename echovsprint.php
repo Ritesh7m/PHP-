f80c2a9fec70f8f9b1fb7c9f3dd54bc04de618b0
Ritesh Maurya <maurya.ritesh2005@gmail.com>
2026-01-11 14:43:17 +0530

    echo vs print

---
+++ echovsprint.php
@@ -0,0 +1,23 @@
+<?php
+// echo "ECHO Vs PRINT"; 
+/* 
+echo                 |         print 
+echo is faster than  |print is slower than
+print                |echo 
+no return value      |return value is 1
+can display multipal |display only one value
+value                |
+can use with or      |can use with or without
+without()parentheses |parentheses ()
+ */
+$a = "learning php";
+$b = "is fun";
+echo "<h2>PHP is Fun </h2> <br>";
+echo "$a $b<br>";
+
+print "hello in php using print <br>";
+$a ="hello";
+$b = "world";
+print ("$a $b");
+
+?>
