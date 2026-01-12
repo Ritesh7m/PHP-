2329adae60881226d6cdcc6239bdce1ebeeddc01
Ritesh Maurya <maurya.ritesh2005@gmail.com>
2026-01-12 13:04:13 +0530

    conditional statement

---
+++ conditional.php
@@ -0,0 +1,13 @@
+<?php
+// conditional statement execute the block of code when the specific condition meets like--> if age is greater than 18---> can vote else --> can't vote go and do study...
+// some contional stmt are-->  if else switch condition  
+
+$age = 17;
+ if($age>18){
+    echo "you can vote";
+ }
+ else{
+    echo " cant vote go and do study";
+ }
+
+?>
